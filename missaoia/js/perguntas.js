8f80125e2d890d806625a233ee118d9c2b3a4830
Larissadalberto <108533667+Larissadalberto@users.noreply.github.com>
2024-08-29 11:22:16 -0300

    Add files via upload

---
+++ missaoia/js/perguntas.js
@@ -0,0 +1,172 @@
+export const perguntas = [
+    {
+        enunciado: "Como a tecnologia tem impactado a agricultura moderna?",
+        alternativas: [
+            {
+                texto: "Aumentando a eficiência e sustentabilidade através de automação e monitoramento em tempo real.",
+                afirmacao: [
+                    "Maior produtividade: A automação e o monitoramento em tempo real permitem uma gestão mais precisa das lavouras e rebanhos, resultando em colheitas mais eficientes e aumento da produção agrícola.",
+                    "Redução de impactos ambientais: Tecnologias como sensores e drones ajudam a otimizar o uso de recursos como água e fertilizantes, diminuindo o desperdício e minimizando o impacto ambiental da agricultura."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Reduzindo o uso de tecnologias avançadas e promovendo práticas agrícolas tradicionais.",
+                afirmacao: [
+                    "Menor eficiência produtiva: A falta de tecnologias modernas pode levar a práticas menos eficientes, resultando em menor produtividade e aumento do trabalho manual necessário.",
+                    "Aumento dos impactos ambientais: Práticas agrícolas tradicionais podem não ser tão eficazes na gestão de recursos, o que pode levar a um maior uso de água e fertilizantes e, consequentemente, a um impacto ambiental mais significativo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o principal benefício da automação na agricultura?",
+        alternativas: [
+            {
+                texto: "Redução do custo inicial de investimento em tecnologias avançadas.",
+                afirmacao: [
+                    "Menos acesso a inovações: Reduzir o investimento pode limitar a adoção de novas tecnologias que poderiam melhorar a produtividade e a sustentabilidade a longo prazo.",
+                    "Possível perda de competitividade: Sem as tecnologias avançadas, os agricultores podem enfrentar dificuldades em competir com aqueles que utilizam tecnologias modernas, resultando em uma menor participação no mercado.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Melhoria na precisão do manejo das culturas e aumento da eficiência operacional.",
+                afirmacao: [
+                    "Aumento da produtividade: A automação melhora a precisão na aplicação de insumos e na gestão das lavouras, resultando em maiores rendimentos e melhor qualidade das colheitas.",
+                    "Redução de custos operacionais: A eficiência operacional reduz a necessidade de trabalho manual e otimiza o uso de recursos, diminuindo os custos gerais de operação.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um dos principais desafios da integração de tecnologia na agricultura?",
+        alternativas: [
+            {
+                texto: " Aumento da necessidade de treinamento especializado para operar novas tecnologias.",
+                afirmacao: [
+                    "Custos adicionais: O treinamento especializado pode representar um custo extra para os produtores, além de exigir investimento em tempo e recursos para capacitar a equipe.",
+                    "Curva de aprendizado: A implementação de novas tecnologias pode enfrentar resistência inicial e atrasos devido à curva de aprendizado dos operadores, impactando a eficiência no curto prazo.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Maior facilidade na adaptação das técnicas agrícolas tradicionais às novas tecnologias.",
+                afirmacao: [
+                    "Integração mais fluida: A adaptação mais fácil permite uma integração mais rápida e eficiente das tecnologias modernas nos processos tradicionais, potencializando a produtividade.",
+                    "Menos inovação: A facilidade na adaptação pode levar a uma adoção mais conservadora das tecnologias, resultando em menos inovação e aproveitamento dos avanços tecnológicos disponíveis.",
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é um impacto positivo da tecnologia no monitoramento das condições do solo?",
+        alternativas: [
+            {
+                texto: " Aumento da quantidade de fertilizantes usados para compensar a falta de dados precisos.",
+                afirmacao: [
+                    "Impacto ambiental negativo: O uso excessivo de fertilizantes pode levar à poluição do solo e da água, contribuindo para problemas ambientais como a eutrofização de corpos d'água.",
+                    "Custos elevados: A aplicação de mais fertilizantes do que o necessário resulta em custos maiores para os agricultores e pode diminuir a rentabilidade da produção."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Melhoria na gestão de recursos e aplicação precisa de insumos, promovendo uma agricultura mais sustentável.",
+                afirmacao: [
+                    "Redução do impacto ambiental: A aplicação precisa de insumos reduz o uso excessivo de fertilizantes e pesticidas, minimizando a contaminação do solo e da água e promovendo práticas mais ecológicas.",
+                    "Economia de recursos e custos: A gestão eficiente dos recursos permite uma utilização mais econômica de fertilizantes e água, resultando em redução de custos operacionais e aumento da rentabilidade."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como a tecnologia pode melhorar a previsão de condições climáticas para a agricultura?",
+        alternativas: [
+            {
+                texto: "Proporcionando modelos climáticos avançados que ajudam na previsão precisa das condições meteorológicas.",
+                afirmacao: [
+                    "Tomada de decisão mais informada: A previsão precisa permite que os agricultores planejem melhor as atividades, como o plantio e a colheita, reduzindo riscos e otimizando a produção.",
+                    "Redução de perdas: Com informações climáticas mais precisas, é possível implementar medidas preventivas para proteger as culturas contra eventos climáticos adversos, minimizando danos e perdas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Reduzindo a necessidade de coleta de dados meteorológicos ao depender exclusivamente de métodos tradicionais.",
+                afirmacao: [
+                    "Menos precisão nas previsões: Métodos tradicionais podem não fornecer dados detalhados ou atualizados, resultando em previsões climáticas menos precisas e maior risco para as atividades agrícolas.",
+                    "Possível aumento de erros: A falta de tecnologia avançada pode levar a decisões baseadas em informações desatualizadas ou imprecisas, afetando negativamente a gestão das culturas e a produtividade."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como a tecnologia pode influenciar a eficiência no uso da água na agricultura?",
+        alternativas: [
+            {
+                texto: "Como a tecnologia pode influenciar a eficiência no uso da água na agricultura?",
+                afirmacao: [
+                    "Economia de água: A irrigação precisa reduz o desperdício de água, aplicando a quantidade exata necessária para as culturas, o que contribui para a conservação dos recursos hídricos.",
+                    "Aumento da produtividade: Com o monitoramento em tempo real, as plantas recebem a quantidade ideal de água, promovendo um crescimento saudável e melhorando os rendimentos das colheitas."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "incentivando o uso excessivo de água por falta de controle e dados precisos sobre as necessidades das culturas.",
+                afirmacao: [
+                    "Desperdício de água: Sem dados precisos, a irrigação pode ser excessiva, resultando em desperdício de água e sobrecarga dos recursos hídricos disponíveis.",
+                    "Danos ao solo e às culturas: O uso excessivo de água pode levar à salinização do solo e outros problemas, afetando negativamente a saúde das plantas e a produtividade das culturas."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como a tecnologia pode impactar a gestão de pragas e doenças na agricultura?",
+        alternativas: [
+            {
+                texto: "Utilizando sensores e imagens de alta resolução para detectar e identificar pragas e doenças precocemente.",
+                afirmacao: [
+                    "Controle mais eficaz: A detecção precoce permite a aplicação rápida e precisa de tratamentos, reduzindo o impacto das pragas e doenças nas culturas e melhorando a saúde das plantas.",
+                    "Redução do uso de pesticidas: Com a identificação precisa das pragas e doenças, é possível usar menos pesticidas, aplicando-os somente onde são necessários, o que diminui os custos e os impactos ambientais."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Dependendo exclusivamente de métodos tradicionais de controle que podem não identificar problemas de forma eficiente.",
+                afirmacao: [
+                    "Resposta tardia: Métodos tradicionais podem não detectar problemas até que se tornem graves, resultando em danos significativos às culturas antes de serem tratados.",
+                    "Uso excessivo de pesticidas: A falta de precisão nos métodos tradicionais pode levar ao uso indiscriminado de pesticidas, aumentando os custos e potencialmente causando efeitos negativos no meio ambiente e na saúde humana."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como a tecnologia pode influenciar a precisão na gestão da fertilização das culturas?",
+        alternativas: [
+            {
+                texto: " Utilizando sistemas de aplicação variável de fertilizantes baseados em dados do solo para fornecer a quantidade exata de nutrientes necessários.",
+                afirmacao: [
+                    "Melhoria na eficiência dos nutrientes: A aplicação precisa de fertilizantes garante que as plantas recebam a quantidade exata de nutrientes necessários, promovendo um crescimento mais saudável e aumentando o rendimento das colheitas.",
+                    "Redução de custos e impacto ambiental: A aplicação variável minimiza o uso excessivo de fertilizantes, reduzindo os custos com insumos e diminuindo a poluição do solo e da água."
+                ],
+            },
+            {
+                texto: "Aplicando fertilizantes de maneira uniforme em toda a área, independentemente das necessidades específicas do solo.",
+                afirmacao: [
+                    "Desperdício de fertilizantes: A aplicação uniforme pode levar ao uso excessivo de fertilizantes em áreas onde não são necessários, resultando em desperdício de recursos e aumento dos custos operacionais.",
+                    "Impactos ambientais negativos: O uso excessivo de fertilizantes pode causar problemas ambientais, como a poluição de corpos d'água e a degradação do solo, além de afetar a saúde das plantas ao causar desequilíbrios nutricionais.",
+                    
+                ],
+            },
+        ]
+    }
+];
